39cb33c9292d4ea8fef85a7beb85c43b7f4e8ab2
dugbang <dugbang@gmail.com>
2017-10-21 17:36:41 +0900

    Initial commit

---
+++ app/src/main/java/com/example/dugbang/mytab/Fragment2.java
@@ -0,0 +1,23 @@
+package com.example.dugbang.mytab;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by dugbang on 2017-10-15.
+ */
+
+public class Fragment2 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment2, container, false);
+
+        return rootView;
+    }
+}
